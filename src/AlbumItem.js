a31e5cf2f723e81a95a0a2d74253b480d07ed9b5
Ryan Warner <ryan.warner.us@gmail.com>
2022-07-19 14:14:00 +0200

    Testing with gradients

--- src/AlbumItem.js
+++ src/AlbumItem.js
@@ -8,13 +8,13 @@ function AlbumItem(props) {
     useEffect(() =>{
         const imageWidth = imageRef.current.clientWidth;
         const imageHeight = imageRef.current.clientHeight;
-        AR = props.albumExpanded ? Math.max(imageWidth / imageHeight, imageHeight / imageWidth): 1
+        AR = props.albumExpanded ? Math.max(imageWidth / imageHeight, imageHeight / imageWidth): 0.9
         imageRef.current.style.transform = `scaleX(${AR}) scaleY(${AR})`
     },[props.albumExpanded])
 
     return (
         <div className="w-full aspect-square flex items-center justify-center overflow-hidden">
-            <div className="w-full h-32 aspect-video shadow-xl bg-blue-400 rounded-xl duration-200 ease-in" ref={imageRef}></div>
+            <div className="w-full h-32 aspect-video shadow-xl bg-gradient-to-r from-purple-400 via-green-500 to-pink-500 rounded-xl duration-200 ease-in" ref={imageRef}></div>
         </div>
     )
 }
