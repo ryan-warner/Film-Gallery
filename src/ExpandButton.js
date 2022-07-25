ad70962e5b28f7468605618eea6645e97b4e4d52
Ryan Warner <ryan.warner.us@gmail.com>
2022-07-25 10:30:55 +0200

    CHanged ExpandButton behavior

--- src/ExpandButton.js
+++ src/ExpandButton.js
@@ -4,10 +4,10 @@ import { ChevronDownIcon } from "@heroicons/react/outline";
 
 function ExpandButton(props) {
     return (
-        <button onClick={props.setAlbumExpanded} className="flex aspect-square items-center relative h-full">
-            <div className={(props.albumExpanded ? "h-full w-full " : "h-[40%] w-full ") + " duration-200 rounded-lg border-4 border-primaryAccent ease-in"}></div>
-            {props.albumExpanded ? <ChevronDownIcon className="absolute inset-x-0 duration-200 h-1/3 w-full top-0 my-[2px] stroke-[4px] stroke-primaryAccent" /> : <ChevronUpIcon className="absolute inset-x-0 duration-200  h-1/3 w-full top-0 stroke-[4px] stroke-primaryAccent" />}
-            {props.albumExpanded ? <ChevronUpIcon className="absolute inset-x-0 duration-200 h-1/3 w-full bottom-0 my-[2px] stroke-[4px] stroke-primaryAccent" /> : <ChevronDownIcon className="absolute inset-x-0 duration-200  h-1/3 w-full bottom-0 stroke-[4px] stroke-primaryAccent" />}
+        <button onClick={props.setAlbumExpanded} className="flex aspect-[3/4] items-center relative h-full">
+            <div className={"h-[40%] w-full duration-200 rounded-lg border-4 border-primaryAccent ease-in"}></div>
+            {props.albumExpanded ? <ChevronDownIcon className="absolute inset-x-0 duration-200 h-1/3 w-full top-0 stroke-[4px] stroke-primaryAccent" /> : <ChevronUpIcon className="absolute inset-x-0 duration-200  h-1/3 w-full top-0 stroke-[4px] stroke-primaryAccent" />}
+            {props.albumExpanded ? <ChevronUpIcon className="absolute inset-x-0 duration-200 h-1/3 w-full bottom-0 stroke-[4px] stroke-primaryAccent" /> : <ChevronDownIcon className="absolute inset-x-0 duration-200  h-1/3 w-full bottom-0 stroke-[4px] stroke-primaryAccent" />}
         </button>
     )
 }
--- src/Library.js
+++ src/Library.js
@@ -1,10 +1,37 @@
 import Album from "./Album"
 import PortfolioHeader from "./PortfolioHeader";
 import SideBar from "./SideBar"
-import { useState } from "react";
+import { useState, useEffect  } from "react";
 
 function Library(props) {
     const [selectedAlbum, setSelectedAlbum] = useState("My Library")
+    const [albums, setAlbums] = useState()
+
+    useEffect(() => {
+        const result = async () => {
+            try {
+                const response = await fetch("/api/v1/albums", {
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                        "Authorization": "Bearer " + localStorage.getItem("filmstripToken")
+                    },
+                    method: "GET",
+                    mode: "cors"
+                })
+                if (response.ok) {
+                    console.log(response)
+                    return response
+                }
+            } catch {
+                return null;
+            }
+            console.log("Hi")
+        }
+        result();
+        console.log()
+    })
 
     return (
         <div className="py-6 px-8 bg-primaryColor h-full relative">
