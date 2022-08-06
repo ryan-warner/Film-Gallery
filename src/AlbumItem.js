8e21975f142b3d82dda746e390e85b93d8eb737e
Ryan Warner <ryan.warner.us@gmail.com>
2022-08-06 13:53:01 +0200

    Add state represention for added/removed favorite

--- src/AlbumItem.js
+++ src/AlbumItem.js
@@ -1,9 +1,11 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { HeartIcon } from "@heroicons/react/outline";
 
 function AlbumItem(props) {
     const imageRef = useRef(null)
-    var AR = 1
+    const [isFavorite, setIsFavorite] = useState()
+    const [updateFavorite, setUpdateFavorite] = useState(false)
+    var AR = 0.9
 
     useEffect(() =>{
         const imageWidth = imageRef.current.clientWidth;
@@ -12,6 +14,36 @@ function AlbumItem(props) {
         imageRef.current.style.transform = `scaleX(${AR}) scaleY(${AR})`
     },[props.albumExpanded])
 
+    useEffect(() => {
+        if (updateFavorite) {
+            const favorite = isFavorite ===  undefined ? props.thumbnail.favorite : isFavorite
+            const photoID = props.thumbnail.imageID
+            const result = fetch(
+                "/api/v1/photo/favorite",
+                {
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                        "Authorization": "Bearer " + localStorage.getItem("filmstripToken")
+                    },
+                    method: "POST",
+                    mode: "cors",
+                    body: JSON.stringify(
+                        {
+                        "setFavorite": !favorite,
+                        "photoID": photoID
+                    })
+                }
+            )
+            result.then((response) => response.json()).then((body) => (setIsFavorite(body.isFavorite), setUpdateFavorite(false)))
+        }
+    }, [updateFavorite])
+
+    function handleFavorite() {
+        setUpdateFavorite(true)
+    }
+
     return (
         <div className="w-full aspect-square flex items-center justify-center overflow-hidden relative group">
             {
@@ -19,8 +51,9 @@ function AlbumItem(props) {
                 <img src={"data:image/jpeg;base64," + props.thumbnail.image} className={(props.thumbnail.orientation === "portrait" ? "h-full": "w-full") + " rounded-xl duration-200 ease-in"} ref={imageRef}></img>
             }
             <div className="absolute inset-0">
-            <p>{!props.thumbnail.favorite ? "TRUE" : "FALSE"}</p>
-                <HeartIcon className="invisible group-hover:visible h-5 aspect-square absolute bottom-0 right-0 m-2 fill-red-500 stroke-red-500" />
+                <button title={(props.thumbnail.favorite ? "Remove from Favorites" : "Add to Favorites")} onClick={() => handleFavorite()}>
+                    <HeartIcon className={(isFavorite !== undefined ? (isFavorite ? "visible fill-white" : "invisible group-hover:visible fill-transparent") : (props.thumbnail.favorite ? "visible fill-white" : "invisible group-hover:visible fill-transparent")) + " h-5 aspect-square absolute bottom-0 right-0 m-2 stroke-white"} />
+                </button>
             </div>
         </div>
     )
