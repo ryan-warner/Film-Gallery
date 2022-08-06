ec2b80b39a9e3e91d1598c00164ef8298ced2232
Ryan Warner <ryan.warner.us@gmail.com>
2022-08-06 19:06:42 +0200

    Updated sidebar to accomodate album queries

--- src/SideBar.js
+++ src/SideBar.js
@@ -14,19 +14,19 @@ function SideBar(props) {
             <div className="flex flex-col gap-1 text-xl text-left pb-2">
                 <div className={(props.selectedAlbum === "My Library" ? "bg-gray-300" : "bg-transparent") + " flex gap-1 justify-start items-center rounded-md px-2 py-1"}>
                     <CollectionIcon className="h-6 aspect-square" />
-                    <button className="w-min min-w-fit" onClick={() => props.onClick("My Library")}>My Library</button>
+                    <button className="w-min min-w-fit" onClick={() => props.onClick("My Library", null)}>My Library</button>
                 </div>
                 <div className={(props.selectedAlbum === "Favorites" ? "bg-gray-300" : "bg-transparent") + " flex gap-1 justify-start items-center rounded-md px-2 py-1"}> 
                     <HeartIcon className="h-6 aspect-square" />
-                    <button className="w-min min-w-fit" onClick={() => props.onClick("Favorites")}>Favorites</button>
+                    <button className="w-min min-w-fit" onClick={() => props.onClick("Favorites", null)}>Favorites</button>
                 </div>
                 <div className={(props.selectedAlbum === "Portfolio" ? "bg-gray-300" : "bg-transparent") + " flex gap-1 justify-start items-center rounded-md px-2 py-1"}> 
                     <FilmIcon className="h-6 aspect-square" />
-                    <button className="w-min min-w-fit" onClick={() => props.onClick("Portfolio")}>Portfolio</button>
+                    <button className="w-min min-w-fit" onClick={() => props.onClick("Portfolio", null)}>Portfolio</button>
                 </div>
             </div>
             <p className="font-semibold">Albums</p>
-            {albums.map((item, index) => <SideBarItem selectedAlbum={props.selectedAlbum} label={item.albumName} key={index} onClick={props.onClick} />)}
+            {albums.map((item, index) => <SideBarItem selectedAlbum={props.selectedAlbum} albumID={item.albumID} label={item.albumName} key={index} onClick={props.onClick} />)}
         </div>
     )
 }
--- src/SideBarItem.js
+++ src/SideBarItem.js
@@ -3,7 +3,7 @@ function SideBarItem(props) {
         <div className={(props.selectedAlbum === props.label ? "bg-gray-300" : "bg-transparent") + " flex gap-1 justify-start items-center rounded-md px-2 py-1"}>
             <button
                 className="text-xl truncate w-full text-left"
-                onClick={() => props.onClick(props.label)}
+                onClick={() => props.onClick(props.label, props.albumID)}
                 disabled={props.disabled ? true : false}
             >
                 {props.label}
