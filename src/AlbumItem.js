f26a0350f2f69e2325c10cfb10129efdab7efd5a
Ryan Warner <ryan.warner.us@gmail.com>
2022-08-06 15:38:49 +0200

    Fixed favorite icon to bottom right corner

--- src/AlbumItem.js
+++ src/AlbumItem.js
@@ -3,6 +3,7 @@ import { HeartIcon } from "@heroicons/react/outline";
 
 function AlbumItem(props) {
     const imageRef = useRef(null)
+    const buttonRef = useRef(null)
     const [isFavorite, setIsFavorite] = useState()
     const [updateFavorite, setUpdateFavorite] = useState(false)
     var AR = 0.9
@@ -12,6 +13,9 @@ function AlbumItem(props) {
         const imageHeight = imageRef.current.clientHeight;
         AR = props.albumExpanded ? Math.max(imageWidth / imageHeight, imageHeight / imageWidth): 0.9
         imageRef.current.style.transform = `scaleX(${AR}) scaleY(${AR})`
+        buttonRef.current.style.width = AR === 0.9 ? `${imageWidth * 0.9}px`: `100%`
+        buttonRef.current.style.height = AR === 0.9 ? `${imageHeight * 0.9}px`: `100%`
+        
     },[props.albumExpanded])
 
     useEffect(() => {
@@ -47,12 +51,13 @@ function AlbumItem(props) {
     return (
         <div className="w-full aspect-square flex items-center justify-center overflow-hidden relative group">
             {
-                props.thumbnail.type === "png" ? <img src={"data:image/png;base64," + props.thumbnail.image} className={(props.thumbnail.orientation === "portrait" ? "h-full": "w-full") + " rounded-xl duration-200 ease-in"} ref={imageRef}></img> : 
-                <img src={"data:image/jpeg;base64," + props.thumbnail.image} className={(props.thumbnail.orientation === "portrait" ? "h-full": "w-full") + " rounded-xl duration-200 ease-in"} ref={imageRef}></img>
+                props.thumbnail.type === "png" ? 
+                    <img src={"data:image/png;base64," + props.thumbnail.image} className={(props.thumbnail.orientation === "portrait" ? "h-full": "w-full") + " rounded-xl duration-200 ease-in"} ref={imageRef}></img>: 
+                    <img src={"data:image/jpeg;base64," + props.thumbnail.image} className={(props.thumbnail.orientation === "portrait" ? "h-full": "w-full") + " rounded-xl duration-200 ease-in"} ref={imageRef}></img>
             }
-            <div className="absolute inset-0">
-                <button title={(props.thumbnail.favorite ? "Remove from Favorites" : "Add to Favorites")} onClick={() => handleFavorite()}>
-                    <HeartIcon className={(isFavorite !== undefined ? (isFavorite ? "visible fill-white" : "invisible group-hover:visible fill-transparent") : (props.thumbnail.favorite ? "visible fill-white" : "invisible group-hover:visible fill-transparent")) + " h-5 aspect-square absolute bottom-0 right-0 m-2 stroke-white"} />
+            <div ref={buttonRef} className={"top-1/2 -translate-x-1/2 left-1/2 -translate-y-1/2 duration-200 ease-in absolute"}>
+                <button className="absolute h-5 m-2 aspect-square bottom-0 right-0"title={(props.thumbnail.favorite ? "Remove from Favorites" : "Add to Favorites")} onClick={() => handleFavorite()}>
+                    <HeartIcon className={(isFavorite !== undefined ? (isFavorite ? "visible fill-white" : "invisible group-hover:visible fill-transparent") : (props.thumbnail.favorite ? "visible fill-white" : "invisible group-hover:visible fill-transparent")) + " h-5 aspect-square stroke-white"} />
                 </button>
             </div>
         </div>
